f24c631d8eebcee4d47c3d31e04ee687c3e98e8f
nischay <nischey.me008@gmail.com>
2024-05-19 20:02:49 +0530

    added on 19 May 2024

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,13 +1,105 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function App() {
-    return (
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
 
-        <h1>Hello React !</h1>
+
+function App() {
+    const x = "Jonas";
+    return (<div>
+        <Header />
+        <Menu />
+        <Footer />
+    </div>
     )
 }
 
+function Header() {
+    return <h1>Fast React Pizza Co.</h1>
+}
+
+
+function Menu() {
+
+    return (
+        <div>
+            <h2>Our Menu</h2>
+            <Pizza />
+            <Pizza />
+            <Pizza />
+            <Pizza />
+        </div>
+    )
+}
+
+function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 8;
+    const CloseHour = 18;
+
+    if (hour >= openHour && hour <= CloseHour) {
+        alert("We are Currently Open");
+    } else {
+        alert("We are Closed!");
+    }
+    return (
+        <footer>{new Date().toLocaleTimeString()}.We're currently Open</footer>
+    )
+}
+
+
+function Pizza() {
+    return (
+        <div>
+            <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+            <h2>Pizza Margherita</h2>
+            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+        </div>
+    )
+}
 
 // React  V18
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -17,5 +109,8 @@ root.render(
         <App />
     </React.StrictMode>
 );
+
+
+
 
 // React Before 18
