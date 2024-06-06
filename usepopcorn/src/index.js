bfd63d778aad13603ea013e16ee21b767d1ac865
nischay <nischey.me008@gmail.com>
2024-06-06 18:42:42 +0530

    added on 06 June 2024

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -7,9 +7,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test /> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
