29bd8e92ca2e50759cc7aed1a7d892f852219af8
nischay <nischey.me008@gmail.com>
2024-06-15 02:19:22 +0530

    Added on 15 June 2024

---
+++ classy-weather/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+  }
+
+
+
+  handleDecrement(){
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={}>-</button>
+        <span>{this.state.count}</span>
+        <button>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
---
+++ usepopcorn/filter.js
@@ -0,0 +1,5 @@
+const items = [1, 2, 3, 4, 5, 6];
+
+const filterItems = items.filter((e) => e > 3);
+
+console.log(filterItems);
