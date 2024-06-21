bc5ca6c036108a893d1497107850c9f5261919a1
nischay <nischey.me008@gmail.com>
2024-06-22 00:58:13 +0530

    added on  22 june 2024

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -3,15 +3,18 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="product" element={<Product />} />
+          <Route path="/" element={<Homepage />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="Homepage" element={<Homepage />} />
           <Route path="*" element={<PageNotFound />} />
+          <Route path="app" element={<AppLayout />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ worldwise/src/Components/AppNav.jsx
@@ -0,0 +1,7 @@
+import styles from "./AppNav.modules.css";
+
+function AppNav() {
+  return <nav className={styles.nav}>App Nav Components</nav>;
+}
+
+export default AppNav;
