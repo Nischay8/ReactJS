b82988238bcafbb536cb8eb92360c69279f9151d
nischay <nischey.me008@gmail.com>
2024-07-11 00:06:21 +0530

    Added on 11 July 2024

--- redux-intro/src/features/customers/customerSlice.js
+++ redux-intro/src/features/customers/customerSlice.js
@@ -1,26 +1,57 @@
-const intialStateCustomer = {
+/* eslint-disable no-undef */
+import { createSlice } from "@reduxjs/toolkit";
+
+const intialState = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
 
-export default function customerReducer(state = intialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/creatCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
+const customerSlice = createSlice({
+  name: "customer",
+  intialState,
+  reducers: {
+    creatCustomer: {
+      prepare: function (fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
 
-    case "customer/updateName":
-      return { ...state, fullName: action.payload.fullName };
+      reducer(state, action) {
+        // eslint-disable-next-line no-undef
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
 
-    default:
-      return { ...state };
-  }
-}
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+// export default function customerReducer(state = intialState, action) {
+//   switch (action.type) {
+//     case "customer/creatCustomer":
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         nationalID: action.payload.nationalID,
+//         createdAt: action.payload.createdAt,
+//       };
+
+//     case "customer/updateName":
+//       return { ...state, fullName: action.payload.fullName };
+
+//     default:
+//       return { ...state };
+//   }
+// }
 
 export function creatCustomer(fullName, nationalID) {
   return {
