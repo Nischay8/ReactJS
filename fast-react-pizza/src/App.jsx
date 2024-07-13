7ddf68bd1237ecfcc1fdd4357305f45854e9ed3d
nischay <nischey.me008@gmail.com>
2024-07-14 01:03:01 +0530

    Added on 14 July 2024

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuloader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 
@@ -18,6 +18,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: menuloader,
       },
       {
         path: "/cart",
